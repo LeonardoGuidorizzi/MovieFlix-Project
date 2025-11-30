2c6fb20aa3c9456d44e06c96a08ddfa8c5851270
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-30 16:52:37 -0300

    feat: add Streaming mapper

--- src/main/java/br/com/movieflix/movieflix/entity/mapper/StreamingMapper.java
+++ src/main/java/br/com/movieflix/movieflix/entity/mapper/StreamingMapper.java
@@ -1,4 +1,25 @@
 package br.com.movieflix.movieflix.entity.mapper;
 
+import br.com.movieflix.movieflix.entity.Streaming;
+import br.com.movieflix.movieflix.entity.dto.streaming.StreamingResponse;
+import br.com.movieflix.movieflix.entity.dto.streaming.StreamingRequest;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class StreamingMapper {
+    public static Streaming toEntity (StreamingRequest request){
+        return Streaming
+                .builder()
+                .name(request.name())
+                .build();
+
+    }
+
+    public static StreamingResponse toDto (Streaming entity){
+        return StreamingResponse
+                .builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
+    }
 }
