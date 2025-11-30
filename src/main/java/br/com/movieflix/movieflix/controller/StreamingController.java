bb4a68e840075b210362f6f6b932445274b54cc6
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-30 16:41:47 -0300

    feat: add Streaming Controller

--- src/main/java/br/com/movieflix/movieflix/controller/StreamingController.java
+++ src/main/java/br/com/movieflix/movieflix/controller/StreamingController.java
@@ -1,4 +1,12 @@
 package br.com.movieflix.movieflix.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movieflix/streaming")
 public class StreamingController {
+
+
 }
