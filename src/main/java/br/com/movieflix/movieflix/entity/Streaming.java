bfe852ebfdb18d54bb66d009e9d7eecc62cd32ef
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-30 18:51:43 -0300

    feat: add Column notion just for patter

--- src/main/java/br/com/movieflix/movieflix/entity/Streaming.java
+++ src/main/java/br/com/movieflix/movieflix/entity/Streaming.java
@@ -14,5 +14,6 @@ public class Streaming {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String name;
 }
