982ba663c92561223497dcc0b2d72759f8d52095
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-09 18:15:54 -0300

    feat: add user mapper

--- src/main/java/br/com/movieflix/movieflix/entity/mapper/UserMapper.java
+++ src/main/java/br/com/movieflix/movieflix/entity/mapper/UserMapper.java
@@ -1,4 +1,26 @@
 package br.com.movieflix.movieflix.entity.mapper;
 
+import br.com.movieflix.movieflix.entity.User;
+import br.com.movieflix.movieflix.entity.dto.User.UserReponse;
+import br.com.movieflix.movieflix.entity.dto.User.UserRequest;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UserMapper {
+    public static User toEntity (UserRequest request){
+        return User
+                .builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .build();
+    }
+    public static UserReponse toDto (User entity){
+        return UserReponse
+                .builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .email(entity.getEmail())
+                .build();
+    }
 }
