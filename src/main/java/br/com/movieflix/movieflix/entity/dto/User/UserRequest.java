85ab430dd4408f07b61dae6b8bbf057dc10c727c
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-09 18:15:16 -0300

    feat: add user request dto

--- src/main/java/br/com/movieflix/movieflix/entity/dto/User/UserRequest.java
+++ src/main/java/br/com/movieflix/movieflix/entity/dto/User/UserRequest.java
@@ -1,4 +1,7 @@
 package br.com.movieflix.movieflix.entity.dto.User;
 
-public record UserRequest() {
+import lombok.Builder;
+
+@Builder
+public record UserRequest(String name, String email, String password) {
 }
