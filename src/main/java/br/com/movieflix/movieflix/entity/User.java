da711487d259223ae7e033c69058789a477e6cab
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-09 18:02:58 -0300

    feat: add user entity and the attributes

--- src/main/java/br/com/movieflix/movieflix/entity/User.java
+++ src/main/java/br/com/movieflix/movieflix/entity/User.java
@@ -1,4 +1,24 @@
 package br.com.movieflix.movieflix.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user" )
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
 }
