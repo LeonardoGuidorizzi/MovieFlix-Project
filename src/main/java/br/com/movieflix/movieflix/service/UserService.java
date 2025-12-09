b1a6d65b2a459e451153e5460c7e09e6569dfb53
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-09 18:26:04 -0300

    feat: git add user service

--- src/main/java/br/com/movieflix/movieflix/service/UserService.java
+++ src/main/java/br/com/movieflix/movieflix/service/UserService.java
@@ -1,4 +1,19 @@
 package br.com.movieflix.movieflix.service;
 
+import br.com.movieflix.movieflix.entity.dto.User.UserReponse;
+import br.com.movieflix.movieflix.entity.dto.User.UserRequest;
+import br.com.movieflix.movieflix.entity.mapper.UserMapper;
+import br.com.movieflix.movieflix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository repository;
+
+    public UserReponse create (UserRequest request){
+        return UserMapper.toDto(repository.save(UserMapper.toEntity(request)));
+    }
 }
