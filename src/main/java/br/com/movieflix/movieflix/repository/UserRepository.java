270200db4be4352c8bbbe3ce7aafbf27f647d9c2
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-19 11:33:33 -0300

    feat: add UserRepository to register and find a user to make the log in flow

--- src/main/java/br/com/movieflix/movieflix/repository/UserRepository.java
+++ src/main/java/br/com/movieflix/movieflix/repository/UserRepository.java
@@ -3,5 +3,8 @@ package br.com.movieflix.movieflix.repository;
 import br.com.movieflix.movieflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String userRegister);
 }
