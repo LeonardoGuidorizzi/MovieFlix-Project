5e49394a0dd247c5e18a54f92fbea45a929578d8
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-19 11:30:59 -0300

    feat: add RegisterResponse dto

--- src/main/java/br/com/movieflix/movieflix/entity/dto/User/RegisterReponse.java
+++ src/main/java/br/com/movieflix/movieflix/entity/dto/User/RegisterReponse.java
@@ -3,5 +3,5 @@ package br.com.movieflix.movieflix.entity.dto.User;
 import lombok.Builder;
 
 @Builder
-public record UserReponse(Long id, String name, String email) {
+public record RegisterReponse(Long id, String name, String email) {
 }
