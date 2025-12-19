1a17f239c13599d95864f35d3940174d7dd5dfa2
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-19 11:25:15 -0300

    feat: add auth controller to create and log in the user

--- src/main/java/br/com/movieflix/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/movieflix/controller/AuthController.java
@@ -1,12 +1,19 @@
 package br.com.movieflix.movieflix.controller;
 
-import br.com.movieflix.movieflix.entity.dto.User.UserReponse;
-import br.com.movieflix.movieflix.entity.dto.User.UserRequest;
+import br.com.movieflix.movieflix.config.TokenService;
+import br.com.movieflix.movieflix.entity.User;
+import br.com.movieflix.movieflix.entity.dto.User.LoginRequest;
+import br.com.movieflix.movieflix.entity.dto.User.LoginResponse;
+import br.com.movieflix.movieflix.entity.dto.User.RegisterReponse;
+import br.com.movieflix.movieflix.entity.dto.User.RegisterRequest;
 import br.com.movieflix.movieflix.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("movieflix/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    @Autowired
-    private UserService userService;
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserReponse> createUser (@RequestBody UserRequest request){
+    public ResponseEntity<RegisterReponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authentication.getPrincipal();
+        String token = tokenService.generateToken(user);
+        return ResponseEntity.ok(new LoginResponse(token));
     }
 
 
