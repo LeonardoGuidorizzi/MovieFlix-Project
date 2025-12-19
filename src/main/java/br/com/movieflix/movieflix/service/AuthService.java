1c97b402291ec82bcddc95be5d436a4c279c3e4b
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-12-19 11:39:01 -0300

    feat: add auth service to make the login

--- src/main/java/br/com/movieflix/movieflix/service/AuthService.java
+++ src/main/java/br/com/movieflix/movieflix/service/AuthService.java
@@ -1,4 +1,18 @@
 package br.com.movieflix.movieflix.service;
 
-public class AuthService {
+import br.com.movieflix.movieflix.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+    @Autowired
+    UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String userRegister) throws UsernameNotFoundException {
+        return repository.findByEmail(userRegister).orElseThrow(()-> new RuntimeException("Password or email could be wrong"));
+    }
 }
