ce00736aeed99576ab31a1707a87a17bf0709187
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-01-21 17:56:30 -0300

    refactor: change roles on authService

--- src/main/java/br/com/movieflix/movieflix/security/auth/AuthService.java
+++ src/main/java/br/com/movieflix/movieflix/security/auth/AuthService.java
@@ -1,24 +1,34 @@
-package br.com.movieflix.movieflix.service;
+package br.com.movieflix.movieflix.security.auth;
 
-import br.com.movieflix.movieflix.config.TokenService;
-import br.com.movieflix.movieflix.repository.UserRepository;
+import br.com.movieflix.movieflix.entity.User;
+import br.com.movieflix.movieflix.entity.dto.Auth.LoginRequest;
+import br.com.movieflix.movieflix.entity.dto.Auth.LoginResponse;
+import br.com.movieflix.movieflix.security.jwt.TokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService implements UserDetailsService {
-    @Autowired
-    UserRepository repository;
+public class AuthService{
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
-    @Override
-    public UserDetails loadUserByUsername(String userRegister) throws UsernameNotFoundException {
-        return repository.findByEmail(userRegister).orElseThrow(()-> new RuntimeException("Password or email could be wrong"));
+
+    public LoginResponse login (LoginRequest request){
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                );
+
+        Authentication authentication =
+                authenticationManager.authenticate(authToken);
+
+        User user = (User) authentication.getPrincipal();
+        String token = tokenService.generateToken(user);
+        return  new LoginResponse(token);
     }
+
 }
