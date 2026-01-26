8a27cdd0259ca32f313728d1df76c555bb8bfd3b
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-01-26 16:00:08 -0300

    feat: add the name repeat validation

--- src/main/java/br/com/movieflix/movieflix/repository/MovieRepository.java
+++ src/main/java/br/com/movieflix/movieflix/repository/MovieRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+    boolean existsByNameIgnoreCase(String name);
     List<Movie> findMovieByCategories_Id(Long categoriesId);
     List<Movie> findMovieByStreamings_Id(Long streamingsId);
 }
--- src/main/java/br/com/movieflix/movieflix/repository/StreamingRepository.java
+++ src/main/java/br/com/movieflix/movieflix/repository/StreamingRepository.java
@@ -4,4 +4,5 @@ import br.com.movieflix.movieflix.domain.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StreamingRepository extends JpaRepository<Streaming, Long> {
+    boolean existsByNameIgnoreCase(String name);
 }
