f542d51d7a01946fd03fe63cba737f990f446213
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-01-28 00:01:52 -0300

    feat: add ErrorResponse

--- src/main/java/br/com/movieflix/movieflix/exception/validation/response/ErrorResponse.java
+++ src/main/java/br/com/movieflix/movieflix/exception/validation/response/ErrorResponse.java
@@ -1,8 +1,11 @@
-package br.com.movieflix.movieflix.exception;
+package br.com.movieflix.movieflix.exception.validation.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime timestamp,
         int status,
         String error,
