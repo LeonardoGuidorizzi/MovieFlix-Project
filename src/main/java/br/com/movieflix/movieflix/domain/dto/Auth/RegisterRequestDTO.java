f3f88d1a51d34be78b45dbd574058bc80d0d0a72
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-02 10:40:22 -0300

    feat: add notBlank field on request body

--- src/main/java/br/com/movieflix/movieflix/domain/dto/Auth/RegisterRequestDTO.java
+++ src/main/java/br/com/movieflix/movieflix/domain/dto/Auth/RegisterRequestDTO.java
@@ -1,7 +1,14 @@
 package br.com.movieflix.movieflix.domain.dto.Auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
-public record RegisterRequestDTO(String name, String email, String password) {
+public record RegisterRequestDTO(
+        @NotBlank(message = "Your name is required.")
+        String name,
+        @NotBlank(message = "Your email address is required.")
+        String email,
+        @NotBlank(message = "Your password is required.")
+        String password) {
 }
