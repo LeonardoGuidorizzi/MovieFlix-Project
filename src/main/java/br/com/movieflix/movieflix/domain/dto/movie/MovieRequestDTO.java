f4b833e8df2d89fb7ee2310bfb90f4b431dee67c
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-02-05 14:30:14 -0300

    hotfix: add validation on create movie

--- src/main/java/br/com/movieflix/movieflix/domain/dto/movie/MovieRequestDTO.java
+++ src/main/java/br/com/movieflix/movieflix/domain/dto/movie/MovieRequestDTO.java
@@ -1,5 +1,9 @@
 package br.com.movieflix.movieflix.domain.dto.movie;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,9 +13,15 @@ public record MovieRequestDTO(
         String description,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+        @PastOrPresent
         LocalDate releaseDate,
-
+        @NotNull
+        @NotBlank
         Double rating,
+        @NotNull
+        @NotEmpty
         List<Long> categories,
+        @NotNull
+        @NotEmpty
         List<Long> streamings
 ) {}
